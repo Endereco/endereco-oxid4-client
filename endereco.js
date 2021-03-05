5267c157b5693c00cdb28284a9b3763b5fc69136
Ilja <info@ilja-weber.de>
2021-03-05 12:35:06 +0100

    feat: add fallback function for salutation setter

--- endereco.js
+++ endereco.js
@@ -105,6 +105,20 @@ EnderecoIntegrator.resolvers.salutationRead = function (value) {
     });
 }
 
+EnderecoIntegrator.resolvers.salutationSetValue = function (subscriber, value) {
+    if (
+        !!$ &&
+        subscriber.object &&
+        subscriber.object.classList.contains('selectpicker') &&
+        !!$(subscriber.object).selectpicker
+    ) {
+        $(subscriber.object).selectpicker('val', value);
+    } else {
+        subscriber.object.value = value;
+    }
+}
+
+
 EnderecoIntegrator.afterAMSActivation.push( function(EAO) {
     //
 
